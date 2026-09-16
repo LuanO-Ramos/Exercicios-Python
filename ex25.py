62dd37215f8955778460a9064b73a9bc428eec47
Luan Ramos <eulu4nramosoliveira@gmail.com>
2026-09-16 16:30:46 -0400

    ex25.py

---
+++ ex25.py
@@ -0,0 +1,7 @@
+pequenas = int(input("Digite a quantidade de camisetas pequenas: "))
+medias = int(input("Digite a quantidade de camisetas médias: "))
+grandes = int(input("Digite a quantidade de camisetas grandes: "))
+
+total = (pequenas * 10) + (medias * 12) + (grandes * 15)
+
+print("Valor total da compra: R$", total)
