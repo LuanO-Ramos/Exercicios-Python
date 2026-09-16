eb02c018ad3b29b41ed09dae16fca8bd5738f4a4
Luan Ramos <eulu4nramosoliveira@gmail.com>
2026-09-16 16:39:57 -0400

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+tituloEleitoral = input("Digite seu título eleitoral: ")
+numeroCandidato = input("igite seu número: ")
+
+print("Nome: ", nome)
+print("Título eleitoral: ", tituloEleitoral)
+print("Número do candidato: ", numeroCandidato)
