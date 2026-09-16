259feb9e2b92ed864fa4d3a6474678f00f28ff67
Luan Ramos <eulu4nramosoliveira@gmail.com>
2026-09-16 16:37:58 -0400

    ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Digite a nota do primeiro bimestre: "))
+nota2 = float(input("Digite a nota do segundo bimestre: "))
+nota3 = float(input("Digite a nota do terceiro bimestre: "))
+nota4 = float(input("Digite a nota do quarto bimestre: "))
+
+print("Primeiro bimestre: ", nota1)
+print("Segundo bimestre: ", nota2)
+print("Terceiro bimestre: ", nota3)
+print("Quarto bimestre: ", nota4)
