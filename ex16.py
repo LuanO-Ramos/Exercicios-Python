91bd7173d4da81515c843ea92b72e94246621f59
Luan Ramos <eulu4nramosoliveira@gmail.com>
2026-09-16 16:23:24 -0400

    ex16.py

---
+++ ex16.py
@@ -0,0 +1,20 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+if(numero1 > numero2 and numero1 > numero3):
+    maior = numero1
+elif(numero2 > numero1 and numero2 > numero3):
+    maior = numero2
+else:
+    maior = numero3
+
+if(numero1 < numero2 and numero1 < numero3):
+    menor = numero1
+elif(numero2 < numero1 and numero2 < numero3):
+    menor = numero2
+else:
+    menor = numero3
+
+print("Maior:", maior)
+print("Menor:", menor)
